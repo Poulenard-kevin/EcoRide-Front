1212729a8877aa9b873b6d854531800c4fee96c8
Kevin Poulenard <poulenard.kevin@gmail.com>
2025-09-06 08:39:42 +0200

    Ajout fichier Html/scss/js et rajout de route sur allRoutes + @import 'employee-space' sur le fichier main

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,6 +4,7 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/accueil", "Accueil", "/pages/home.html"),
     new Route("/covoiturage", "Covoiturage", "/pages/covoiturage.html"),
+    new Route("/espace-employe", "Espace Employé", "/pages/employee-space.html","/Js/employee-space.js","/scss/employee-space.css"),
 
 ];
 

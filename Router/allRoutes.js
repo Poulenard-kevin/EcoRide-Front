950338eccc24477de191e0bd9850152675a9b693
Kevin Poulenard <poulenard.kevin@gmail.com>
2025-09-24 15:46:45 +0200

    Ajout modification allRoutes.js

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -1,9 +1,6 @@
 import Route from "./Route.js";
 
 export const allRoutes = [
-    new Route("/accueil", "Accueil", "/pages/home.html"),
-    new Route("/covoiturage", "Covoiturage", "/pages/covoiturage.html"),
-
   new Route("/accueil", "Accueil", "/pages/home.html", "/Js/home.js"),
   new Route("/covoiturage", "Covoiturage", "/pages/covoiturage.html", "/Js/covoiturage.js", "/scss/_covoiturage.css"),
   new Route("/detail", "Détail trajet", "/pages/detail.html", "/Js/detail.js"),
